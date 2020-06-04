1b4a44327f9681e37bb6bcca4078de019bdb60f8
Anson5951 <message9698@gmail.com>
2020-06-04 11:24:10 +0800

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+import { MenuBarComponent } from './menu-bar/menu-bar.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { TestComponent } from './menu-bar/test/test.component';
+import { HomePageComponent } from './menu-bar/home-page/home-page.component';
+import { MemberListComponent } from './menu-bar/member-list/member-list.component';
+import { MemberDetailComponent } from './menu-bar/member-list/member-detail/member-detail.component';
+import { NewsListComponent } from './menu-bar/home-page/news-list/news-list.component';
+
+@NgModule({
+  imports:      [ BrowserModule, FormsModule ],
+  declarations: [ AppComponent, HelloComponent, MenuBarComponent, LoginPageComponent, TestComponent, HomePageComponent, MemberListComponent, MemberDetailComponent, NewsListComponent ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
